0a00118790c280914f1b4ffd378a9a3d0c3c2c64
Mab <mabchhan@gmail.com>
2023-01-22 19:46:35 -0800

    Fill out route in category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,58 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({ include: [{ model: Product }] }).then((categoryData) => {
+    res.json(categoryData);
+  });
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findByPk(req.params.id, { include: [{ model: Product }] }).then(
+    (categoryData) => {
+      res.json(categoryData);
+    }
+  );
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name,
+  })
+    .then((newCategory) => {
+      res.json(newCategory);
+    })
+    .catch((err) => res.json(err));
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    { category_name: req.body.category_name },
+    { where: { id: req.params.id } }
+  )
+    .then((updateCategory) => {
+      res.json(updateCategory);
+    })
+    .catch((err) => res.json(err));
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({ where: { id: req.params.id } })
+    .then((deletedCategory) => {
+      res.json(deletedCategory);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 module.exports = router;
