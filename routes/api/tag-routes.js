1536e8588dcad175e844b228248b4d629cfa6172
Mab <mabchhan@gmail.com>
2023-01-25 09:43:59 -0800

    updated if delete, put, find by id, not found send message to user in tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -21,6 +21,10 @@ router.get("/:id", (req, res) => {
   // be sure to include its associated Product data
   Tag.findByPk(req.params.id, { include: [{ model: Product }] })
     .then((tagData) => {
+      if (!tagData) {
+        res.status(404).json({ message: "No tag found with this id" });
+        return;
+      }
       res.json(tagData);
     })
     .catch((err) => {
@@ -43,6 +47,10 @@ router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
   Tag.update(req.body, { where: { id: req.params.id } })
     .then((updatedTag) => {
+      if (!updatedTag[0]) {
+        res.status(404).json({ message: "No tag found with this id" });
+        return;
+      }
       res.json(updatedTag);
     })
     .catch((err) => {
@@ -54,6 +62,10 @@ router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
   Tag.destroy({ where: { id: req.params.id } })
     .then((deleteTag) => {
+      if (!deleteTag) {
+        res.status(404).json({ message: "No tag found with this id" });
+        return;
+      }
       res.json(deleteTag);
     })
     .catch((err) => {
